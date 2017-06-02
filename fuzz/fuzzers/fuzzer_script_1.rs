ef5ab2ad678e87970f68a1e6f3b6869d42bcb53b
Frank Denis <github@pureftpd.org>
2017-06-02 14:36:37 +0200

    Include decompression in the current fuzzing test
    Since we don't currently validate compressed rdata in DNSSector, cases
    triggering a panic are expected.

--- fuzz/fuzzers/fuzzer_script_1.rs
+++ fuzz/fuzzers/fuzzer_script_1.rs
@@ -2,9 +2,21 @@
 #[macro_use] extern crate libfuzzer_sys;
 extern crate dnssector;
 
+use compress::*;
 use dnssector::*;
 
-fuzz_target!(|data: &[u8]| {
-    let dns_sector = DNSSector::new(data.to_vec()).unwrap();
-    let _ = dns_sector.parse();
+fuzz_target!(|packet: &[u8]| {
+    let dns_sector = DNSSector::new(packet.to_vec()).unwrap();
+    let parsed = dns_sector.parse();
+    let packet = match parsed {
+        Err(_) => return,
+        Ok(packet) => packet.into_packet()
+    };
+    let uncompressed = match Compress::uncompress(&packet) {
+        Err(_) => {},
+        Ok(packet) => {
+            let dns_sector = DNSSector::new(packet).unwrap();
+            let _ = dns_sector.parse();
+        }
+    };
 });
