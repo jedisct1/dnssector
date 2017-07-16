3d6296eb181a7cf4f673d358a344831b7c276a24
Frederik Deweerdt <frederik.deweerdt@gmail.com>
2017-07-16 11:52:53 -0700

    Fuzz iterator operations

--- fuzz/fuzzers/fuzzer_script_op.rs
+++ fuzz/fuzzers/fuzzer_script_op.rs
@@ -5,11 +5,48 @@ extern crate dnssector;
 use compress::*;
 use dnssector::*;
 
+fn iter_ops(i: &mut DNSIterable, ops: &[u8]) -> u64
+{
+    let mut ret = 0;
+    let mut prev_op = 0 as u8;
+    for op in ops {
+        let res = match *op {
+            0 => {
+                match i.offset() {
+                    Some(x) => x,
+                    None => 0,
+                }
+            },
+            1 => { i.offset_next() },
+            2 => { i.set_offset((prev_op * *op) as usize); 1 },
+            3 => { i.set_offset_next((prev_op * *op) as usize); 1 },
+            4 => { i.invalidate(); 1 },
+            5 => { if i.is_tombstone() { 0 } else { 1 } },
+            6 => { i.recompute_rr(); 1 },
+            7 => { i.recompute_sections();1  },
+            8 => { let p = i.packet(); p[0] as usize },
+            9 => { let slice = i.name_slice(); slice[0] as usize },
+            10 => { let slice = i.rdata_slice(); slice[0] as usize },
+            11 => { let mut slice = i.rdata_slice_mut(); slice[0] /= 2; slice[0] as usize },
+            12 => {
+                match i.uncompress() {
+                    Ok(()) => 1,
+                    _ => 2,
+                }
+            },
+            _ => 1,
+        };
+        ret += res;
+        prev_op = *op;
+    }
+    ret as u64
+}
 fuzz_target!(|input: &[u8]| {
-    if input.len() < 8 {
+    if input.len() < 64 {
         return;
     }
-    let (ops, packet) = input.split_at(8);
+    let (all_ops, packet) = input.split_at(64);
+    let (ops, rr_ops) = input.split_at(32);
     let mut cloned_packet  = packet.to_vec();
     let mut cloned_packet = cloned_packet.as_mut();
     let dns_sector = DNSSector::new(packet.to_vec()).unwrap();
@@ -35,12 +72,42 @@ fuzz_target!(|input: &[u8]| {
             6 => parsed.opcode() as u64,
             7 => {parsed.set_opcode(u8_arg); 1},
             8 => {parsed.recompute(); 1},
-            9 => {parsed.into_iter_question(); 1}
-            10 => {parsed.into_iter_answer(); 1}
-            11 => {parsed.into_iter_nameservers(); 1}
-            12 => {parsed.into_iter_additional(); 1}
-            13 => {parsed.into_iter_additional_including_opt(); 1}
-            14 => {parsed.into_iter_edns(); 1}
+            9 => {
+                match parsed.into_iter_question() {
+                    Some(mut i) => iter_ops(&mut i, rr_ops),
+                    None => 0,
+                }
+            }
+            10 => {
+                match parsed.into_iter_answer() {
+                    Some(mut i) => iter_ops(&mut i, rr_ops),
+                    None => 0,
+                }
+            }
+            11 => {
+                match parsed.into_iter_nameservers() {
+                    Some(mut i) => iter_ops(&mut i, rr_ops),
+                    None => 0,
+                }
+            }
+            12 => {
+                match parsed.into_iter_additional() {
+                    Some(mut i) => iter_ops(&mut i, rr_ops),
+                    None => 0,
+                }
+            }
+            13 => {
+                match parsed.into_iter_additional_including_opt() {
+                    Some(mut i) => iter_ops(&mut i, rr_ops),
+                    None => 0,
+                }
+            }
+            14 => {
+                match parsed.into_iter_edns() {
+                    Some(mut i) => iter_ops(&mut i, rr_ops),
+                    None => 0,
+                }
+            }
             15 => DNSSector::qdcount(cloned_packet) as u64,
             16 => {DNSSector::set_qdcount(cloned_packet, u16_arg); 1}
             17 => DNSSector::ancount(cloned_packet) as u64,
