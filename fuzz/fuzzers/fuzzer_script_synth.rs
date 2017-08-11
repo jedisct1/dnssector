ff3339442dedcfaa6641fc2d45510838a07c6560
Frederik Deweerdt <fdeweerdt@fastly.com>
2017-08-11 11:31:28 -0700

    Corpus + fuzzer for synth parser

---
+++ fuzz/fuzzers/fuzzer_script_synth.rs
@@ -0,0 +1,10 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate dnssector;
+
+use dnssector::synth::gen::RR;
+
+fuzz_target!(|synth: &[u8]| {
+    let synth = String::from_utf8_lossy(synth);
+    RR::from_string(&synth);
+});
