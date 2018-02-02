5d0077856c5b8508b52e894f05a666ad302add78
Frank Denis <github@pureftpd.org>
2018-02-02 21:33:47 +0100

    Format

--- src/errors.rs
+++ src/errors.rs
@@ -1,14 +1,25 @@
 #[derive(Debug, Fail)]
 pub enum DSError {
-    #[fail(display = "Packet too small")] PacketTooSmall,
-    #[fail(display = "Packet too large")] PacketTooLarge,
-    #[fail(display = "Unsupported class: {}", _0)] UnsupportedClass(u16),
-    #[fail(display = "Internal error: {}", _0)] InternalError(&'static str),
-    #[fail(display = "Invalid name in a DNS record: {}", _0)] InvalidName(&'static str),
-    #[fail(display = "Invalid DNS packet: {}", _0)] InvalidPacket(&'static str),
-    #[fail(display = "Unsupported RR type: {}", _0)] UnsupportedRRType(String),
-    #[fail(display = "Void record")] VoidRecord,
-    #[fail(display = "Property not found")] PropertyNotFound,
-    #[fail(display = "Wrong address family")] WrongAddressFamily,
-    #[fail(display = "Parse error")] ParseError,
+    #[fail(display = "Packet too small")]
+    PacketTooSmall,
+    #[fail(display = "Packet too large")]
+    PacketTooLarge,
+    #[fail(display = "Unsupported class: {}", _0)]
+    UnsupportedClass(u16),
+    #[fail(display = "Internal error: {}", _0)]
+    InternalError(&'static str),
+    #[fail(display = "Invalid name in a DNS record: {}", _0)]
+    InvalidName(&'static str),
+    #[fail(display = "Invalid DNS packet: {}", _0)]
+    InvalidPacket(&'static str),
+    #[fail(display = "Unsupported RR type: {}", _0)]
+    UnsupportedRRType(String),
+    #[fail(display = "Void record")]
+    VoidRecord,
+    #[fail(display = "Property not found")]
+    PropertyNotFound,
+    #[fail(display = "Wrong address family")]
+    WrongAddressFamily,
+    #[fail(display = "Parse error")]
+    ParseError,
 }
