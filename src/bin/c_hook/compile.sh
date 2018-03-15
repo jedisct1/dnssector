8dc01072e824ee9db96dbb45a824d78bdb420679
Frank Denis <github@pureftpd.org>
2018-03-15 01:15:45 -0700

    Ignore unused params

--- src/bin/c_hook/compile.sh
+++ src/bin/c_hook/compile.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-exec clang -g -Weverything -Wno-incompatible-pointer-types-discards-qualifiers -dynamiclib -o master.dylib c_hook.c
+exec clang -g -Weverything -Wno-unused -Wno-unused-parameter -Wno-incompatible-pointer-types-discards-qualifiers -dynamiclib -o 42.dylib c_hook.c
